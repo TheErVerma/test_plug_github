047cfe4b1388b43e5fdd0fe488c24cdb4cc96293
TheErVerma <102722902+TheErVerma@users.noreply.github.com>
2022-07-13 16:22:45 +0530

    added initial files

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
 <?php
-
-echo "hello";
-
+include "db.php";
 ?>
+<style>
+    .card_row{
+        max-width: 400px;
+        margin: auto;
+    }
+</style>
+<div class="container">
+    <button class="btn btn-primary">Add Card</button>
+    <div class="row card_row">
+        <div class="col">
+            <div class="card p-2 shadow-sm rounded">
+                <div class="first_name_wrapper">
+                    <span>First name</span>
+                    <input type="text" name="fname" id="fname" class="form-control" />
+                </div>
+                <div class="first_name_wrapper">
+                    <span>Last name</span>
+                    <input type="text" name="lname" id="lname" class="form-control" />
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
